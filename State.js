c653d2f988dc51f451dcb7445f9ddfb92948ae57
Matthew Keracher <keracher@uwm.edu>
2023-08-13 00:24:35 +0200

    sa

--- State.js
+++ State.js
@@ -9,7 +9,7 @@ const State = {
     const rows = Array.from({ length: numEntries }, (_, x) => {
       const columns = Array.from({ length: numEntries }, (_, y) => ({
         name: '',
-        fill: '', //'#2596be',
+        fill: '#2596be',
         text: ''
       }));
       return columns;
